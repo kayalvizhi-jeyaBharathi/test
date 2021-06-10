4eeb6a9f94ae5815ddda9bd7cae90a49fc007c0b
kayalvizhi-jeyabharathi <kayaljvizhi@gmail.com>
2021-06-10 09:33:14 +0530

    Moving test project

---
+++ core/src/main/java/com/mindtree/test/core/models/Admin.java
@@ -0,0 +1,33 @@
+package com.mindtree.test.core.models;
+
+import javax.inject.Inject;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+
+@Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class Admin {
+
+	@Inject
+	private String name;
+
+	@Inject
+	private String email;
+
+	@Inject
+	private boolean isadmin;
+
+	public String getName() {
+		return name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public boolean isIsadmin() {
+		return isadmin;
+	}
+
+}
